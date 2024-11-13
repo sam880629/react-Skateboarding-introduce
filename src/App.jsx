9c67be838b94f502d66daab60710d3f2edac2bbb
sam880629 <sam880629@gmail.com>
2024-11-13 17:13:45 +0800

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import { ParallaxProvider } from "react-scroll-parallax";
 import ToutContent from "./components/ToutContent";
 import NavBar from "./Header/NavBar";
-// import CardCollection from "./components/CardCollection";
-// import ToutOverlap from "./components/ToutOverlap";
-// import BigTextLittleText from "./components/BigTextLittleText";
-// import VerticalLine from "./components/VerticalLine";
-// import CardSlider from "./components/CardSlider";
-// import Pullout from "./components/Pullout";
+import CardCollection from "./components/CardCollection";
+import ToutOverlap from "./components/ToutOverlap";
+import BigTextLittleText from "./components/BigTextLittleText";
+import VerticalLine from "./components/VerticalLine";
+import CardSlider from "./components/CardSlider";
+import Pullout from "./components/Pullout";
+import  ImgSroll from "./GSAP/ImgSroll"
 
 const App = ()=> {
   return (
@@ -15,31 +16,32 @@ const App = ()=> {
       <div className="App">
         <NavBar />
         <div className="w-screen h-screen page-hero" />
-        <ToutContent textDirection="toutContent w-full my-0 mb-4 mx-auto py-20 sm:py-0  flex flex-col-reverse sm:flex-row text-left sm:text-right" />
-        {/* <ToutContent textDirection="toutContent w-full my-0 mb-4 mx-auto py-20 sm:py-0  text-left flex flex-col-reverse sm:flex-row-reverse" />
-        <div className="bg-green w-full">
+        <ImgSroll />
+         {/* <ToutContent textDirection="toutContent w-full my-0 mb-4 mx-auto py-20 sm:py-0  flex flex-col-reverse sm:flex-row text-left sm:text-right" /> */}
+        {/* <ToutContent textDirection="toutContent w-full my-0 mb-4 mx-auto py-20 sm:py-0  text-left flex flex-col-reverse sm:flex-row-reverse" /> */}
+        {/* <div className="bg-green w-full">
           <ToutContent textDirection="toutContent w-full my-0 mx-auto py-20 sm:py-0 flex flex-col-reverse sm:flex-row text-left sm:text-right bg-green" />
-        </div>
-        <div className="bg-purple w-full">
+        </div>  */}
+         {/* <div className="bg-purple w-full">
           <ToutContent textDirection="toutContent w-full my-0 mx-auto py-20 sm:py-0 flex flex-col-reverse sm:flex-row-reverse text-left bg-purple" />
-        </div>
-        <div className="w-full toutContent mx-auto">
+        </div> */}
+        {/* <div className="w-full toutContent mx-auto">
           <ToutOverlap
             toutOverlapContent="tout-overlap mx-auto my-0 bg-left"
             totuOverlapText="toutCopy--overlap w-full ml-auto sm:w-1/2 bg-green"
           />
-        </div>
-        <BigTextLittleText />
-        <VerticalLine />
-        <ToutOverlap
+        </div>   */}
+         {/* <BigTextLittleText />  */}
+         {/* <VerticalLine />  */}
+        {/* <ToutOverlap
           toutOverlapContent="toutContent tout-overlap mx-auto my-0 bg-right "
           totuOverlapText="toutCopy--overlap mr-auto w-full sm:w-1/2 bg-orange"
-        />
-        <div className="flex w-full">
+        /> */}
+        {/* <div className="flex w-full">
           <CardCollection />
-        </div>
-        <CardSlider />
-        <Pullout pulloutBackground="bg-green" /> */}
+        </div> */}
+        {/* <CardSlider /> */}
+        {/* <Pullout pulloutBackground="bg-green" /> */}
       </div>
     </ParallaxProvider>
   );
