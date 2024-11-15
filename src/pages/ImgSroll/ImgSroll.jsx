f0539982b43639a79a4361db5bcf43bfadcda54f
sam880629 <sam880629@gmail.com>
2024-11-15 10:10:55 +0800

    no message

--- src/pages/ImgSroll/ImgSroll.jsx
+++ src/pages/ImgSroll/ImgSroll.jsx
@@ -97,7 +97,7 @@ const ImgScroll = () => {
         <img
           ref={mainImgRef}
           className="w-full h-screen object-cover"
-          src="/src/assets/images/skateBoard10.jpg"
+          src="/assets/images/skateBoard10.jpg"
           alt="Main skateboard"
           style={{ transform: 'translate(0, 0)' }} 
         />
@@ -110,7 +110,7 @@ const ImgScroll = () => {
         >
           <img
             className="  shadow-lg "
-             src="../src/assets/images/skateBoard7.jpg"
+             src="/assets/images/skateBoard7.jpg"
             alt="Left top skateboard"
           />
           <img
