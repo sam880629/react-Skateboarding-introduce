38783b8170441c8d8da4b4fcf521fa1c91942ec8
sam880629 <sam880629@gmail.com>
2024-11-22 16:14:49 +0800

    調整各裝置顯示

--- src/components/ToutContent.jsx
+++ src/components/ToutContent.jsx
@@ -3,14 +3,14 @@ import React, { forwardRef } from 'react';
 
 const ToutContent = forwardRef(({ className, header, content }, ref) => (
     <div ref={ref} className={className}>
-      <div className="toutCopy w-full  ">
+    
         {/* 標題 */}
-        <h1 className="toutHeader mt-5 mb-4 sm:mb-1 sm:mt-0 text-2xl md:text-4xl lg:text-6xl"  style={{ fontFamily: "'Rubik Mono One', sans-serif" }}>{header}</h1>
+        <h1 className="text-[#00cf75] mt-5 md:mb-4 sm:mb-1 sm:mt-0 text-3xl md:text-4xl lg:text-6xl"  style={{ fontFamily: "'Rubik Mono One', sans-serif" }}>{header}</h1>
         {/* 內容 */}
-        <div className="mt-6 mr-3 sm:mx-0 text-[#ccc] text-sm md:text-xl lg:text-2xl">
+        <div className="mt-2 md:mt-6 mr-3 sm:mx-0  w-11/12  text-[#ccc] text-sm md:text-xl lg:text-2xl">
           <p>{content}</p>
         </div>
-      </div>
+      
     </div>
 ));
 
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,17 +16,22 @@ const About = () => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: wrapper,
-        start: "top 60%",
+        start: "top +=100%",
         end: "bottom 100%",
-        scrub: 0.3,
+        scrub: 1,
         // markers: true,
       },
     });
-
+    tl.from(cont1, {
+      opacity: 0,
+      duration: 1,
+      y: "50",
+      ease: "power2.out",
+    });
     tl.to(cont1, {
       opacity: 1,
       duration: 1,
-      y: "50",
+      y: "0",
       ease: "power2.out",
     });
 
@@ -42,28 +47,39 @@ const About = () => {
   return (
     <section
       ref={wrapperRef}
-      className="relative w-full h-screen  overflow-hidden flex flex-col md:flex-row  items-center md:items-start px-8  py-0 md:py-24 md:px-10"
+      className="relative w-full h-screen md:h-[150vh] lg:h-screen overflow-hidden flex flex-col lg:flex-row   justify-center items-center  px-8  py-20 md:pt-32 lg:py-24 md:px-10"
     >
       {/* 左側文字 */}
       <ToutContent
-        className={"opacity-0  md:w-1/2 flex-1  md:mr-0"}
+        className="w-full h-auto flex-1 flex flex-col justify-center lg:w-6/12"
         header={header}
         content={content}
         ref={cont1Ref}
       />
       {/* 右側圖片 */}
-      <div className=" relative h-full flex flex-1 items-center  mx-0 mb-5  md:ml-5 md:mb-20 lg:mb-0" >
-        <div  className="absolute right-5 top-[-18%]  md:top-0 lg:top-0 w-7/12  md:w-9/12 lg:w-5/12   z-10 ml-10">
-            <img src="/assets/images/About3.jpg" alt="placeholder" className=" object-cover shadow-lg" />
-          </div>
-        <div  className="my-auto  w-12/12 ">
-          <img src="/assets/images/About1.jpg" alt="placeholder" className=" object-cover  shadow-lg" />
+      <div className=" relative lg:h-full flex flex-1 items-center  mx-0   md:ml-5 md:mb-20 lg:mb-0">
+        <div className="absolute right-5 top-0  md:top-[-20%] lg:top-0  w-6/12  md:w-5/12 lg:w-5/12   z-10 ml-10">
+          <img
+            src="/assets/images/About3.jpg"
+            alt="placeholder"
+            className=" object-cover shadow-lg"
+          />
         </div>
-        <div  className="absolute right-[-5%]  lg:right-0 bottom-16 lg:bottom-0 z-10     w-6/12 md:w-8/12 lg:w-4/12  ">
-          <img src="/assets/images/About2.jpg" alt="placeholder" className=" object-cover shadow-lg" />
+        <div className="my-auto  w-8/12 ">
+          <img
+            src="/assets/images/About1.jpg"
+            alt="placeholder"
+            className=" object-cover mt-10 shadow-lg"
+          />
+        </div>
+        <div className="absolute right-0  lg:right-0 bottom-[-10%] md:bottom-[-25%] lg:bottom-0 z-10     w-5/12 md:w-5/12 lg:w-4/12  ">
+          <img
+            src="/assets/images/About2.jpg"
+            alt="placeholder"
+            className=" object-cover shadow-lg"
+          />
         </div>
       </div>
-     
     </section>
   );
 };
