4a84aefd61e1848d32cf2dd6960f45c8d7a95602
sam880629 <sam880629@gmail.com>
2024-11-25 18:08:45 +0800

    新增塗鴉介紹區塊

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import NavBar from "./Header/NavBar";
 import ImgSroll from "./pages/ImgSroll/ImgSroll";
 import Block1 from "./pages/Block1/Block1";
+import Block2 from "./pages/Block2/Block2";
 import About from "./pages/About/About";
 
 import History from "./pages/History/History";
@@ -14,7 +15,9 @@ const App = () => {
       
         <Block1 />
         <About />
+      
         <ImgSroll />
+        <Block2 />
         <History />
        
   
